1c861185534f4cef5b9e7b866fe35be8f7bef1e9
Spliterr <63750642+Spliterr@users.noreply.github.com>
2022-02-27 00:16:35 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <MyCalculator />
+  </div>
+</template>
+
+<script>
+import MyCalculator from './components/Calculator.vue'
+
+export default {
+  name: 'App',
+  components: {
+   MyCalculator
+  }
+}
+</script>
+
+<style>
+body {
+  height: 100vh;
+  margin: 0;
+  padding: 0px;
+  width: 100%;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  background-color: #13293d;
+  display: flex;
+  height: 100%;
+}
+</style>
